a6872737a5daa534a39af031246e1c22b8f8f444
Josh Storz <josh.storz@gmail.com>
2015-03-18 15:56:48 -0500

    replacing template text

--- js/login.js
+++ js/login.js
@@ -1,5 +1,8 @@
 function checkLogin() {
 	var userHash = window.localStorage.getItem("userHash");
+	if (userHash === null) {
+		window.location = 'login.html' ;
+	}
 	$.ajax({
 		method : "POST",
 		url : "http://50.57.87.125/pluggs/static_login.php",
@@ -12,4 +15,15 @@ function checkLogin() {
 			return false ;
 		}
 	});
+}
+
+function replaceAll() {
+	var userName = window.localStorage.getItem("userName");
+	var userId = window.localStorage.getItem("userId");
+	var regDate = window.localStorage.getItem("regDate");
+    document.body.innerHTML = document.body.innerHTML
+      .replace(new RegExp('_USERNAME_', "g"), userName)
+      .replace(new RegExp('_USERID_', "g"), userId)
+      .replace(new RegExp('_REGDATE_', "g"), regDate)
+    ;
 }
